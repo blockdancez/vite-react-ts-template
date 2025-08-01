138718b4c3030fc4a10e8a941bbe72d272a1405b
WBBB0730 <1339523637@qq.com>
2025-08-02 05:17:06 +0800

    chore: 新增 build:dev 命令

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig, UserConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+export default defineConfig(({ mode }) => {
+  let build: UserConfig['build'], esbuild: UserConfig['esbuild'], define: UserConfig['define']
+
+  if (mode === 'development') {
+    build = {
+      minify: false,
+    }
+
+    esbuild = {
+      jsxDev: true,
+    }
+
+    define = {
+      'process.env.NODE_ENV': '"development"',
+      __DEV__: 'true',
+    }
+  }
+
+  return {
+    plugins: [react()],
+    build,
+    esbuild,
+    define,
+    optimizeDeps: {
+      exclude: ['lucide-react'],
+    },
+  }
 })
